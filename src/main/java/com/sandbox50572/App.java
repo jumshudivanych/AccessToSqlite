b43de29272f92ea81e93b9b2dba310d96d9f9952
jumshudivanych <ivanlykov028@gmail.com>
2018-01-24 01:13:33 +0400

    Initial commit

---
+++ src/main/java/com/sandbox50572/App.java
@@ -0,0 +1,15 @@
+package com.sandbox50572;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //создание экземпляра класса
+        WorkToSqlite workToSqlite = new WorkToSqlite();
+        workToSqlite.work();
+    }
+}
---
+++ src/main/java/com/sandbox50572/WorkToSqlite.java
@@ -0,0 +1,60 @@
+package com.sandbox50572;
+
+import java.sql.*;
+
+public class WorkToSqlite {
+
+
+public void work() {
+        try {
+
+            //инициализация драивера базы данных
+            Class.forName("org.sqlite.JDBC");
+            //создание подключения
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:sqlite:C:\\idea\\sqlite\\accounts.db");//строка подключения
+            //в строке подключения либо полный путь к фаилу либо
+            //имя фаила если в тои же папке
+            System.out.println("Connected");
+
+            //создание sql запроса
+            Statement stmt = connection.createStatement();
+            stmt.execute("INSERT INTO 'users' ('name', 'url', 'login', 'password') VALUES ('ivan', 'google.ru', 'ivanlykov028@gmail.com', '1234cani');");//TODO тут sql запрос
+            System.out.println("Запись добавлена");
+
+            //добавление записи
+            stmt.execute("INSERT INTO 'users' ('name', 'url', 'login', 'password') VALUES ('ivan', 'google.ru', 'ivanlykov028@gmail.com', '1234cani');");
+            System.out.println("Запись добавлена");
+
+            //Удаление данных из таблицы
+            //stmt.execute("DELETE * FROM users");
+
+            //считывание из таблицы
+            ResultSet resSet = stmt.executeQuery("SELECT * FROM users");
+            while(resSet.next())
+            {
+                int id = resSet.getInt("id");
+                String  name = resSet.getString("name");
+                String  url = resSet.getString("url");
+                String  login = resSet.getString("login");
+                String  password = resSet.getString("password");
+                System.out.println( "ID = " + id );
+                System.out.println( "name = " + name );
+                System.out.println( "url = " + url );
+                System.out.println( "login = " + login );
+                System.out.println( "password = " + password );
+                System.out.println("Таблица выведена");
+
+                //TODO !!! Добавить закрытие базы данных в отдельных методах
+                //connection.close();
+                //stmt.close();
+                //resSet.close();
+
+                System.out.println("Соединения закрыты");
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
